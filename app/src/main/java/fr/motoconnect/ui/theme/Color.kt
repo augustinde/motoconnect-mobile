918b9ec2b1119160d27c26a04e0e54e3df105c2c
Augustin D <42963508+augustinde@users.noreply.github.com>
2023-10-24 00:05:27 +0200

    style: custom theme (#1)

--- app/src/main/java/fr/motoconnect/ui/theme/Color.kt
+++ app/src/main/java/fr/motoconnect/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package fr.motoconnect.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary1 = Color(0xFFFFF7EC)
+val Secondary1 = Color(0xFFEAC28B)
+val Tertiary1 = Color(0xFF464062)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary2 = Color(0xFF464062)
+val Secondary2 = Color(0xFFEAC28B)
+val Tertiary2 = Color(0xFFFFF7EC)
+
--- app/src/main/java/fr/motoconnect/ui/theme/Theme.kt
+++ app/src/main/java/fr/motoconnect/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary1,
+    secondary = Secondary1,
+    tertiary = Tertiary1
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Primary2,
+    secondary = Secondary2,
+    tertiary = Tertiary2
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,7 +41,7 @@ private val LightColorScheme = lightColorScheme(
 fun MotoConnectTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
